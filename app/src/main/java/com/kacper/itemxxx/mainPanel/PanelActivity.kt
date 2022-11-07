a669c50d955c23d5b55d5c5e9520d82dd3e1ec9f
KacperL9 <kacper.litwinow@gmail.com>
2022-11-07 19:23:55 +0100

    fix

--- app/src/main/java/com/kacper/itemxxx/mainPanel/PanelActivity.kt
+++ app/src/main/java/com/kacper/itemxxx/mainPanel/PanelActivity.kt
@@ -12,52 +12,52 @@ import com.kacper.itemxxx.email.EmailActivity
 import com.kacper.itemxxx.scanner.mainactivity.MainActivity
 
 class PanelActivity : AppCompatActivity() {
-    private var _binding: ActivityPanelBinding? = null
-    private val binding get() = _binding!!
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        _binding = ActivityPanelBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        changePassword()
-        logOut()
-        openScanner()
-        openChat()
-        openScreenWithEmail()
-    }
+     private var _binding: ActivityPanelBinding? = null
+     private val binding get() = _binding!!
+     override fun onCreate(savedInstanceState: Bundle?) {
+          super.onCreate(savedInstanceState)
+          _binding = ActivityPanelBinding.inflate(layoutInflater)
+          setContentView(binding.root)
+          changePassword()
+          logOut()
+          openScanner()
+          openChat()
+          openScreenWithEmail()
+     }
 
-    private fun changePassword() {
-        binding.btnChange.setOnClickListener {
-            val intent = Intent(this@PanelActivity, ChangePasswordActivity::class.java)
-            startActivity(intent)
+     private fun changePassword() {
+          binding.btnChange.setOnClickListener {
+               val intent = Intent(this@PanelActivity, ChangePasswordActivity::class.java)
+               startActivity(intent)
+          }
+     }
+
+    private fun openScreenWithEmail() {
+        binding.btnEmail.setOnClickListener {
+            val email = Intent(this@PanelActivity, EmailActivity::class.java)
+            startActivity(email)
         }
     }
 
-//    private fun openScreenWithEmail() {
-//        binding.btnEmail.setOnClickListener {
-//            val email = Intent(this@PanelActivity, EmailActivity::class.java)
-//            startActivity(email)
-//        }
-//    }
+     private fun logOut() {
+          binding.btnLogout.setOnClickListener {
+               FirebaseAuth.getInstance().signOut()
+               startActivity(Intent(this@PanelActivity, LoginActivity::class.java))
+               finish()
+          }
+     }
 
-    private fun logOut() {
-        binding.btnLogout.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            startActivity(Intent(this@PanelActivity, LoginActivity::class.java))
-            finish()
-        }
-    }
+     private fun openScanner() {
+          binding.btnScanner.setOnClickListener {
+               val active = Intent(this@PanelActivity, MainActivity::class.java)
+               startActivity(active)
+          }
+     }
 
-    private fun openScanner() {
-        binding.btnScanner.setOnClickListener {
-            val active = Intent(this@PanelActivity, MainActivity::class.java)
-            startActivity(active)
-        }
-    }
-
-    private fun openChat() {
-        binding.btnChat.setOnClickListener {
-            val chats = Intent(this@PanelActivity, ChatActivity::class.java)
-            startActivity(chats)
-        }
-    }
+     private fun openChat() {
+          binding.btnChat.setOnClickListener {
+               val chats = Intent(this@PanelActivity, ChatActivity::class.java)
+               startActivity(chats)
+          }
+     }
 }
